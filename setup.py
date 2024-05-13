9f06f54083014fe8166d9bb1332e45cbfcb3604f
fynian19 <73427369+fynian19@users.noreply.github.com>
2024-05-13 19:11:28 +0200

    updated dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='remlapreprocesspy',
-    version='1.0.0',
+    version='2.0.0',
     packages=find_packages(),
     install_requires=required_packages,
 )
